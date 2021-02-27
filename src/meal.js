d06c1f7fb23d5de75ea307244e1d1af0e3a69c0a
braedonb98 <brbe3574@colorado.edu>
2021-02-27 10:46:46 -0700

    finished exam

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,50 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return ("Delicious "+ name);
 }
 
+function createMenuItem(name, price, mealType){
+  var menuItem = {};
+  menuItem.name = name;
+  menuItem.price = price;
+  menuItem.type = mealType;
+  return(menuItem);
+}
+
+function addIngredients(string, array){
+  for(i in array)
+  {
+    if(array[i] == string)
+    {
+      return(array);
+    }
+  }
+  array.push(string);
+  return array;
+}
+
+function formatPrice(price){
+  return("$"+price);
+}
+
+function decreasePrice(price){
+  return(price*0.9);
+}
+
+function createRecipe(title, ingredients, menuItemType){
+  var recipe = {};
+  recipe.title = title;
+  recipe.ingredients = ingredients;
+  recipe.type = menuItemType;
+  return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
--- src/order.js
+++ src/order.js
@@ -1,7 +1,51 @@
+function takeOrder(order, array)
+{
+  if (array.length>=3)
+  {
+    return order;
+  }
+  array.push(order);
+  return order;
+}
+
+function refundOrder(orderNumber, deliveryOrders)
+{
+  for(i in deliveryOrders)
+  {
+    if (deliveryOrders[i].orderNumber == orderNumber)
+    {
+      deliveryOrders.splice(i,1);
+    }
+  }
+  return (deliveryOrders);
+}
+
+function listItems(array)
+{
+  var string = ""
+ for(i in array)
+ {
+    string= string + array[i].item + ", ";
+ }
+ 
+ return(string.substring(0,string.length - 2));
+}
+
+function searchOrder(array, item)
+{
+  for(i in array)
+  {
+    if(array[i].item == item)
+    {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
